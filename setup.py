c11cd793c3c2751275a731fbdd5bb403b0801154
lizhiqiang <lizhiqiang@idea.edu.cn>
2024-12-05 14:10:11 +0800

    update version to 0.3.3

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def find_packages(pkg_dir: str):
 
 
 setup(name="dds-cloudapi-sdk",
-      version="0.3.2",
+      version="0.3.3",
       description=description,
       long_description=long_description,
       long_description_content_type="text/markdown",
