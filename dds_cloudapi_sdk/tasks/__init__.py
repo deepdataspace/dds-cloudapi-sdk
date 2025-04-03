e651fae3ec4234ede70684d186d50993a52b90db
shijiawei <shijiawei@idea.edu.cn>
2025-04-03 11:01:22 +0800

    delete v1 API

--- dds_cloudapi_sdk/tasks/__init__.py
+++ dds_cloudapi_sdk/tasks/__init__.py
@@ -19,41 +19,10 @@ Here's how to utilize a task class effectively:
 
 """
 
-from dds_cloudapi_sdk.tasks import types
 from dds_cloudapi_sdk.tasks.base import LabelTypes
 from dds_cloudapi_sdk.tasks.base import TaskStatus
-from dds_cloudapi_sdk.tasks.detection import DetectionModel
-from dds_cloudapi_sdk.tasks.detection import DetectionTarget
-from dds_cloudapi_sdk.tasks.detection import DetectionTask
-from dds_cloudapi_sdk.tasks.gsam import BaseGSAMTask
-from dds_cloudapi_sdk.tasks.gsam import TinyGSAMTask
-from dds_cloudapi_sdk.tasks.ivp import IVPTask
-from dds_cloudapi_sdk.tasks.prompt import *
-from dds_cloudapi_sdk.tasks.trex_embd_customize import TRexEmbdCustomize
-from dds_cloudapi_sdk.tasks.trex_embd_infer import TRexEmbdInfer
-from dds_cloudapi_sdk.tasks.trex_generic import TRexGenericInfer
-from dds_cloudapi_sdk.tasks.trex_interactive import TRexInteractiveInfer
 
 __all__ = [
     "TaskStatus",
     "LabelTypes",
-    "TextPrompt",
-    "RectPrompt",
-    "BatchPointPrompt",
-    "BatchRectPrompt",
-    "BatchEmbdPrompt",
-    "BatchPointInfer",
-    "BatchRectInfer",
-    "BatchEmbdInfer",
-    "IVPTask",
-    "TinyGSAMTask",
-    "BaseGSAMTask",
-    "TRexGenericInfer",
-    "TRexInteractiveInfer",
-    "TRexEmbdCustomize",
-    "TRexEmbdInfer",
-    "DetectionTask",
-    "DetectionModel",
-    "DetectionTarget",
-    "types"
 ]
--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def find_packages(pkg_dir: str):
 
 
 setup(name="dds-cloudapi-sdk",
-      version="0.4.0",
+      version="0.4.1",
       description=description,
       long_description=long_description,
       long_description_content_type="text/markdown",
